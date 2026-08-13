6fb5497bb66e94fbc1ef10e2677392f7e1d5fdaf
Adelvis Camacaro <adelvismartin@gmail.com>
2026-08-13 01:41:34 -0500

    qa: turn emulator harness into anonymous YouTube WebView cookie primer

--- insave-attestation-test/app/build.gradle.kts
+++ insave-attestation-test/app/build.gradle.kts
@@ -8,16 +8,12 @@ android {
         applicationId = "com.adelvis.insave.attestationtest"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-}
-
-dependencies {
-    implementation(files("libs/attestation-agent.jar"))
 }
